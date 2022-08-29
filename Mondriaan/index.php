05afbe7f4d9abda1a8089eddaf929fd64e023c8e
Henri <42139@hoornbeeck.nl>
2022-08-29 13:36:36 +0200

    PUSH MONDRIAAN

---
+++ Mondriaan/index.php
@@ -0,0 +1,74 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Mondriaan</title>
+
+    <script src="https://cdn.tailwindcss.com"></script>
+    <link rel="stylesheet" href="mondriaan.css">
+
+</head>
+<body>
+
+
+<div class="grid gap-1 h-screen">
+
+<!-- Full width column -->
+<div class="flex flex-grow gap-1">
+  <div class="flex-grow bg-white"></div>
+  <div class="flex-grow bg-white"></div>
+  <div class="flex-grow bg-yellow-300"></div>
+  <div class="flex-grow bg-blue-500"></div>
+  <div class="flex flex-row flex-grow">
+    <div class="flex-grow bg-yellow-300"></div>
+    <div class="flex-grow bg-black"></div>
+  </div>
+  <div class="flex-grow bg-white"></div>
+</div>
+
+<!-- Two columns -->
+<div class="flex gap-1">
+  <div class="flex-grow bg-red-500"></div>
+  <div class="flex-grow bg-black"></div>
+</div>
+
+<!-- Three columns -->
+<div class="flex gap-1">
+  <div class="flex-grow bg-yellow-300"></div>
+  <div class="flex-grow bg-blue-500"></div>
+  <div class="flex-grow bg-red-500"></div>
+</div>
+
+<!-- Four columns -->
+<div class="flex gap-1">
+  <div class="flex-grow bg-red-500"></div>
+  <div class="flex-grow bg-blue-500"></div>
+  <div class="flex flex-col flex-grow">
+    <div class="flex-grow bg-blue-500"></div>
+    <div class="flex-grow bg-yellow-300"></div>
+  </div>
+  <div class="flex-grow bg-white"></div>
+</div>
+
+<!-- Five columns -->
+<div class="flex gap-1">
+  <div class="flex-grow bg-white"></div>
+  <div class="flex-grow bg-blue-500"></div>
+  <div class="flex-grow bg-black"></div>
+</div>
+
+<!-- Six columns -->
+<div class="flex gap-1">
+  <div class="flex-grow bg-yellow-300"></div>
+  <div class="flex-grow bg-yellow-300"></div>
+  <div class="flex-grow bg-white"></div>
+  <div class="flex-grow bg-red-500"></div>
+  <div class="flex-grow bg-white"></div>
+</div>
+
+</div>
+
+</body>
+</html>
