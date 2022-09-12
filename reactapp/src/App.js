3ef5da786ebb9046e63aa105d1bf6bec2649c219
Henri <42139@hoornbeeck.nl>
2022-09-12 14:20:38 +0200

    react

---
+++ reactapp/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import Navbar from './Navbar/Navbar';
+import Container from './Container/container';
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+      <Container />
+    </div>
+  );
+}
+
+export default App;
---
+++ reactapp/src/Container/container.js
@@ -0,0 +1,21 @@
+import logo from '../logo.svg';
+import './container.css';
+import CardComponent from '../CardComponent/card';
+
+
+function Container() {
+  return (
+    <div className="flex flex-wrap justify-center p-10 gap-5">
+    <div class="grid grid-cols-4 gap-4 content-center">
+        <CardComponent titel="hallo" />
+        <CardComponent titel="doei" />
+        <CardComponent titel="meneer" />
+        <CardComponent titel="drostor" />
+        <CardComponent titel="Daan" />
+        <CardComponent titel="Daan" />
+        </div>
+    </div>
+  );
+}
+
+export default Container;
